0d0e8abbc8e3daa511e6edbada89048a0db88f6d
AKozin <kai.dlaser@gmail.com>
2024-09-14 16:21:42 +0300

    homework "Синтаксис языка и базовые типы данных"

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number_two = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (number+number_two) + "\n" + "Разность чисел: " + (number - number_two) + "\n" + "Произведение чисел: " + (number_two*number) + "\n" + "Частность чисел: " + ((double)number/(double)number_two));
 
     }
 
